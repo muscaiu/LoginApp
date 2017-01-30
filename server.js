24814064be05c0363740bf783b108bb1bfd0c80f
Cristian Muscalu <muscaiu@yahoo.com>
2017-01-30 19:31:38 +0200

    root

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 3000
+var morgan = require('morgan')
+var mongoose = require('mongoose')
+
+app.use(morgan('dev')) //morgan logs every server resqest
+
+app.get('/home', function(req, res) {
+    res.send('hello world')
+})
+
+mongoose.connect('mongodb://localhost:27017/loginapp', function(err) {
+    if (err) {
+        console.log('not connected to the db', err)
+    } else {
+        console.log('connected to Mongo');
+    }
+})
+
+app.listen(port, function() {
+    console.log('server is running on', port)
+})
