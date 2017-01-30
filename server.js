5ef47ad0e7aa06fe531e007c05561a5ce61cc5b8
Cristian Muscalu <muscaiu@yahoo.com>
2017-01-30 20:33:47 +0200

    fixed bodyParser, added express.Router

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ var app = express()
 var port = process.env.PORT || 3000
 var morgan = require('morgan')
 var mongoose = require('mongoose')
-
 var bodyParser = require('body-parser')
+var router = express.Router()
+    //use the router object with these routes ..like a function with (routes) as parameter i guess
+var appRoutes = require('./app/routes/api')(router)
 
 app.use(morgan('dev')) //morgan logs every server resqest
-app.use(bodyParser)
+app.use(bodyParser.json()) //for parsing the json 
+app.use(bodyParser.urlencoded({ extended: true })) //for parse application/x-www-form-urlencoded
+app.use(appRoutes)
 
 mongoose.connect('mongodb://localhost:27017/loginapp', function(err) {
     if (err) {
