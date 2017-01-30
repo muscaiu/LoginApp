b39699513765b437e08855831f6b4af006e383e8
Cristian Muscalu <muscaiu@yahoo.com>
2017-01-30 20:46:11 +0200

    loading paths to the front-end

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ var bodyParser = require('body-parser')
 var router = express.Router()
     //use the router object with these routes ..like a function with (routes) as parameter i guess
 var appRoutes = require('./app/routes/api')(router)
+var path = require('path')
 
 app.use(morgan('dev')) //morgan logs every server resqest
 app.use(bodyParser.json()) //for parsing the json 
 app.use(bodyParser.urlencoded({ extended: true })) //for parse application/x-www-form-urlencoded
-app.use(appRoutes)
+app.use(express.static(__dirname + '/public'))
+app.use('/api', appRoutes) //to deconflict te backend and the frontend routes
 
 mongoose.connect('mongodb://localhost:27017/loginapp', function(err) {
     if (err) {
@@ -19,6 +21,10 @@ mongoose.connect('mongodb://localhost:27017/loginapp', function(err) {
     } else {
         console.log('connected to Mongo');
     }
+})
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/app/views/index.html'))
 })
 
 app.listen(port, function() {
