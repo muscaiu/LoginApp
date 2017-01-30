68c0244be70b4c5627875dbf405b2e992508ca4d
Cristian Muscalu <muscaiu@yahoo.com>
2017-01-30 19:59:33 +0200

    encrypting the pass before storing it to DB

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+var bcrypt = require('bcrypt-nodejs')
 var UserSchema = new Schema({
     username: { type: String, lowercase: true, required: true, unique: true },
     password: { type: String, required: true },
     email: { type: String, required: true, lowercase: true, unique: true }
+})
+
+UserSchema.pre('save', function(next) {
+    var user = this
+        //encrypting the pass before storing it to th    
+    bcrypt.hash(user.password, null, null, function(err, hash) {
+        if (err) return next(err)
+        user.password = hash
+        next()
+    })
 })
 
 module.exports = mongoose.model('User', UserSchema)
