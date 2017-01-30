3e109c26271c9aa839988748f80de9040c65b52a
Cristian Muscalu <muscaiu@yahoo.com>
2017-01-30 20:27:49 +0200

    moving the post request to the routes module api

---
+++ app/routes/api.js
@@ -0,0 +1,27 @@
+var User = require('../models/user')
+
+module.exports = function(router) {
+    //http://127.0.0.1:3000/users
+    router.post('/users', function(req, res) {
+        var user = new User()
+
+        user.username = req.body.username
+        user.password = req.body.password
+        user.email = req.body.email
+
+        if (req.body.username === null || req.body.username == '' ||
+            req.body.password === null || req.body.username == '' ||
+            req.body.email === null || req.body.email === '') {
+            res.send('Ensure username, email and password were provided')
+        } else {
+            user.save(function(err) {
+                if (err) { //if user exists in the db or some other error
+                    res.send('Username or Email already exists')
+                } else {
+                    res.send('user created')
+                }
+            })
+        }
+    })
+    return router //return whatever the route is
+}
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express()
 var port = process.env.PORT || 3000
 var morgan = require('morgan')
 var mongoose = require('mongoose')
-var User = require('./app/models/user')
+
 var bodyParser = require('body-parser')
 
 app.use(morgan('dev')) //morgan logs every server resqest
@@ -14,29 +14,6 @@ mongoose.connect('mongodb://localhost:27017/loginapp', function(err) {
         console.log('not connected to the db', err)
     } else {
         console.log('connected to Mongo');
-    }
-})
-
-//http://127.0.0.1:3000/users
-app.post('/users', function(req, res) {
-    var user = new User()
-
-    user.username = req.body.username
-    user.password = req.body.password
-    user.email = req.body.email
-
-    if (req.body.username === null || req.body.username == '' ||
-        req.body.password === null || req.body.username == '' ||
-        req.body.email === null || req.body.email === '') {
-        res.send('Ensure username, email and password were provided')
-    } else {
-        user.save(function(err) {
-            if (err) { //if user exists in the db or some other error
-                res.send('Username or Email already exists')
-            } else {
-                res.send('user created')
-            }
-        })
     }
 })
 
