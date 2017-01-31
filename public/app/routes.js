e1f769520a6ec2cb7d5603a3cdbc2ab5fc614823
Cristian Muscalu <muscaiu@yahoo.com>
2017-01-31 10:24:15 +0200

    crated, added and tested register.html

--- public/app/routes.js
+++ public/app/routes.js
@@ -8,6 +8,9 @@ angular.module('appRoutes', ['ngRoute'])
         .when('/about', {
             templateUrl: 'app/views/pages/about.html'
         })
+        .when('/register', {
+            templateUrl: 'app/views/pages/users/register.html'
+        })
         .otherwise({ redirectTo: '/' });
 
     $locationProvider.html5Mode({
