303da5b7804087ccc6b0514a8c1c7b70d06b49f9
Cristian Muscalu <muscaiu@yahoo.com>
2017-01-31 10:19:17 +0200

    ! Created Home and About Route look carefully

--- public/app/routes.js
+++ public/app/routes.js
@@ -1,5 +1,17 @@
 angular.module('appRoutes', ['ngRoute'])
 
-.config(function() {
-    console.log('testing the routes');
-})
+.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'app/views/pages/home.html'
+        })
+        .when('/about', {
+            templateUrl: 'app/views/pages/about.html'
+        })
+        .otherwise({ redirectTo: '/' });
+
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    })
+});
